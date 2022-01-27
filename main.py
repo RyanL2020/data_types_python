369312b68325a82db08ddaad9f2a40a12f4980a2
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-01-27 23:30:11 +0000

    data types

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #Data Types
+
+
+# string 
+# subscripting-taking an index of a string 
+print("Hello"[4])
+
+print("123" + "456")
+
+# integer whole number
+print(123 + 345)
+# for large number use _ for ,'s 
+
+# float - floating point number 
+3.14159
+
+# boolean 
+True
+False
